ee0899092700282efd14ad308a121b5896aaa796
来日可期 <zhoukai100123@163.com>
2020-11-10 17:00:26 +0800

    初始化目录结构-引入Vant

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <i class="iconfont icon-shipin"></i>
+    <van-button loading type="primary" loading-type="spinner" />
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 加载全局样式
 import './styles/index.less'
+
+// 加载 Vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
